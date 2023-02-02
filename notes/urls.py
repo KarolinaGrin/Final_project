4a43b3cecb4fe1860f2cb4e69c7fdc2bf05dc895
Karolina Grin <karolina.griniute@codeacademylt.omicrosoft.com>
2023-02-02 20:54:02 +0200

    setting up urls and views

--- notes/urls.py
+++ notes/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import index
+from . import views
 
-urlpatterns = [
-    path('', index, name='index ')
+urlpatterns =[
+    path('',views.index, name='index'),
+    path('login/',views.login, name='login'),
+    path('home/', views.home_page, name='home'),
+    path('edit/',views.edit, name='edit'),
+    path('register/',views.register, name='register'),
+    path('settings/',views.settings, name='settings'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -4,4 +4,23 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'notes/index.html')
+
+
+def register(request):
+    return render(request, 'notes/register.html')
+
+def login (request):
+    return render(request, 'notes/login.html')
+
+def home_page(request):
+    return render(request, 'notes/home.html')
+
+def settings(request):
+    return render(request, 'notes/settings.html')
+
+def logout(request):
+    return render(request,'notes/logout.html')
+
+def edit(request):
+    return render(request, 'notes/edit.html')
